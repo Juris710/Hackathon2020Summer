50791272d7b217585a92b38c9e8142c72e8ca964
Juris710 <50994088+Juris710@users.noreply.github.com>
2020-09-22 13:44:47 +0900

    質問一覧 & 質問内容表示実装

--- lib/shared/utils.dart
+++ lib/shared/utils.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
 
 List<T> castToList<T>(dynamic l) {
   return l.cast<T>() as List<T>;
@@ -6,4 +9,15 @@ List<T> castToList<T>(dynamic l) {
 
 String getDateString(DateTime date) {
   return DateFormat('yyyy年MM月dd日 HH:mm').format(date);
+}
+
+void navigate(BuildContext context, Widget page, List<dynamic> values) {
+  Navigator.of(context).push(
+    MaterialPageRoute(builder: (context) {
+      return MultiProvider(
+        providers: values.map((e) => Provider.value(value: e)).toList(),
+        child: page,
+      );
+    }),
+  );
 }
