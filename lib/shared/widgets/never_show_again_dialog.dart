94509b424f11d3ac15858a4826788396cda6253a
Juris710 <yuu00.juris@gmail.com>
2020-09-23 23:05:55 +0900

    「次回から表示しない」ダイアログ実装
    UniversityGroup登録の際の確認を「次回から表示しない」ができるように

---
+++ lib/shared/widgets/never_show_again_dialog.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class NeverShowAgainDialog extends StatefulWidget {
+  final Widget title;
+  final Widget content;
+  final List<Widget> Function(bool) actions;
+
+  NeverShowAgainDialog({this.title, this.content, this.actions});
+
+  @override
+  _NeverShowAgainDialogState createState() => _NeverShowAgainDialogState();
+}
+
+class _NeverShowAgainDialogState extends State<NeverShowAgainDialog> {
+  bool neverShowAgain = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: widget.title,
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          widget.content,
+          Row(
+            children: [
+              Checkbox(
+                value: neverShowAgain,
+                onChanged: (val) {
+                  setState(() {
+                    neverShowAgain = val;
+                  });
+                },
+              ),
+              Text('次回から表示しない'),
+            ],
+          ),
+        ],
+      ),
+      actions: widget.actions(neverShowAgain),
+    );
+  }
+}
