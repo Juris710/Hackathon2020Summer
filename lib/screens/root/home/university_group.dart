436c5bc6806eec1e8f0a7e7d55e2f64928984b5c
Juris710 <50994088+Juris710@users.noreply.github.com>
2020-09-25 19:28:46 +0900

    質問リスト登録画面に検索機能を実装

--- lib/screens/root/home/university_group.dart
+++ lib/screens/root/home/university_group.dart
@@ -4,6 +4,7 @@ import 'package:hackathon_2020_summer/models/university/question_target.dart';
 import 'package:hackathon_2020_summer/models/university/university_group.dart';
 import 'package:hackathon_2020_summer/models/user/registered_item.dart';
 import 'package:hackathon_2020_summer/screens/root/home/new_question_target.dart';
+import 'package:hackathon_2020_summer/screens/searcher.dart';
 import 'package:hackathon_2020_summer/services/database.dart';
 import 'package:hackathon_2020_summer/shared/widgets/loading.dart';
 
@@ -29,7 +30,94 @@ class UniversityGroup extends StatelessWidget {
               return LoadingScaffold();
             }
             final group = snapshot.data;
-            return Scaffold(
+            return Searcher<QuestionTargetModel>(
+              getSearchTargets: group.questionTargets.snapshots().map((event) =>
+                  event.docs
+                      .map((doc) => QuestionTargetModel.fromFirestore(doc))
+                      .toList()),
+              appBar: AppBar(
+                title: RichText(
+                  textAlign: TextAlign.center,
+                  text: TextSpan(
+                      text: "質問リスト登録",
+                      style: TextStyle(fontSize: 20),
+                      children: <TextSpan>[
+                        if (group?.name != null)
+                          TextSpan(
+                            text: '\n${group?.name}',
+                            style: TextStyle(
+                              fontSize: 16,
+                            ),
+                          ),
+                      ]),
+                ),
+                actions: [
+                  FlatButton.icon(
+                    onPressed: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => NewQuestionTarget(
+                            universityGroupReference: groupReference,
+                          ),
+                        ),
+                      );
+                    },
+                    icon: Icon(Icons.add, color: Colors.white),
+                    label: Text('追加', style: TextStyle(color: Colors.white)),
+                  ),
+                ],
+              ),
+              notFoundWidgets: [
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        '0件です',
+                        style: Theme.of(context).textTheme.headline6,
+                      ),
+                    ],
+                  ),
+                )
+              ],
+              searchTargetsEmptyWidgets: [
+                Expanded(
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text(
+                          '質問リストが存在しません。',
+                          style: Theme.of(context).textTheme.headline6,
+                        ),
+                        Text('右上の「追加」ボタンを押すことで追加できます。'),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+              inputLabelText: '検索',
+              matches: (item, input) => item.name.contains(input),
+              itemBuilder: (item) {
+                final contains =
+                    registeredItem.questionTargets.contains(item.reference);
+                return CheckboxListTile(
+                  title: Text(item.name),
+                  controlAffinity: ListTileControlAffinity.leading,
+                  value: contains,
+                  onChanged: (value) {
+                    if (contains) {
+                      registeredItem.questionTargets.remove(item.reference);
+                    } else {
+                      registeredItem.questionTargets.add(item.reference);
+                    }
+                    registeredItemReference.update(
+                        {'question_targets': registeredItem.questionTargets});
+                  },
+                );
+              },
+            );
+/*            return Scaffold(
               appBar: AppBar(
                 title: RichText(
                   textAlign: TextAlign.center,
@@ -122,7 +210,7 @@ class UniversityGroup extends StatelessWidget {
                   );
                 },
               ),
-            );
+            );*/
           },
         );
       },
