087049714bae73d4f4f9218d0561cc365262f77c
Juris710 <yuu00.juris@gmail.com>
2020-10-07 21:40:17 +0900

    Navigator遷移時にRootページで一瞬データなしと表示される問題の修正

--- lib/screens/root/root.dart
+++ lib/screens/root/root.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hackathon_2020_summer/main.dart';
 import 'package:hackathon_2020_summer/models/user/account.dart';
 import 'package:hackathon_2020_summer/screens/root/edit_account.dart';
 import 'package:hackathon_2020_summer/services/authenticate.dart';
@@ -6,12 +7,22 @@ import 'package:hackathon_2020_summer/shared/constants.dart';
 import 'package:hackathon_2020_summer/shared/widgets/loading.dart';
 import 'package:provider/provider.dart';
 
-class Root extends StatelessWidget {
+class Root extends StatefulWidget {
   Root({Key key}) : super(key: key);
+  @override
+  _RootState createState() => _RootState();
+}
 
+class _RootState extends State<Root> {
+  Account account;
   @override
   Widget build(BuildContext context) {
-    final account = context.watch<Account>();
+    final currentAccount = context.watch<Account>();
+    if (getAuthStatus(currentAccount) == AuthStatus.USER) {
+      setState(() {
+        account = currentAccount;
+      });
+    }
     if (account == null) {
       return LoadingScaffold();
     }
