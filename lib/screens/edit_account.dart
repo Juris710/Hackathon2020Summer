bbf605091c9bb6dd445bed82757386a2d99a624b
Juris710 <yuu00.juris@gmail.com>
2020-10-19 15:29:42 +0900

    アカウント編集で設定時にLoadingを表示するように

--- lib/screens/edit_account.dart
+++ lib/screens/edit_account.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:hackathon_2020_summer/services/database.dart';
 import 'package:hackathon_2020_summer/shared/constants.dart';
+import 'package:hackathon_2020_summer/shared/widgets/loading.dart';
 import 'package:provider/provider.dart';
 
 class EditAccount extends StatefulWidget {
@@ -19,12 +20,14 @@ class EditAccount extends StatefulWidget {
 class _EditAccountState extends State<EditAccount> {
   final _formKey = GlobalKey<FormState>();
   String name;
+  bool loading;
 
   @override
   void initState() {
     super.initState();
     setState(() {
       name = widget.name;
+      loading = false;
     });
   }
 
@@ -34,52 +37,60 @@ class _EditAccountState extends State<EditAccount> {
       appBar: AppBar(
         title: Text('アカウント情報登録'),
       ),
-      body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                TextFormField(
-                  initialValue: name,
-                  decoration: textFieldDecoration.copyWith(
-                    labelText: 'ユーザー名',
-                    prefixIcon: Icon(Icons.person),
-                  ),
-                  validator: (val) => val.isEmpty ? '必須項目です' : null,
-                  onChanged: (val) {
-                    setState(() {
-                      name = val;
-                    });
-                  },
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: Container(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    TextFormField(
+                      initialValue: name,
+                      decoration: textFieldDecoration.copyWith(
+                        labelText: 'ユーザー名',
+                        prefixIcon: Icon(Icons.person),
+                      ),
+                      validator: (val) => val.isEmpty ? '必須項目です' : null,
+                      onChanged: (val) {
+                        setState(() {
+                          name = val;
+                        });
+                      },
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    ElevatedButton(
+                      onPressed: () async {
+                        FocusScope.of(context).unfocus();
+                        if (!_formKey.currentState.validate()) {
+                          return;
+                        }
+                        setState(() {
+                          loading = true;
+                        });
+                        final user = FirebaseAuth.instance.currentUser;
+                        if (name != null) {
+                          await user.updateProfile(displayName: name);
+                        }
+                        await context.read<DatabaseService>().updateUserData(
+                              user.uid,
+                              name: name ?? user.displayName,
+                            );
+                      },
+                      child: Text(widget.submitButtonText ?? ''),
+                    ),
+                  ],
                 ),
-                SizedBox(
-                  height: 10.0,
-                ),
-                ElevatedButton(
-                  onPressed: () async {
-                    FocusScope.of(context).unfocus();
-                    if (!_formKey.currentState.validate()) {
-                      return;
-                    }
-                    //TODO：Loadingウィジェット表示
-                    final user = FirebaseAuth.instance.currentUser;
-                    if (name != null) {
-                      await user.updateProfile(displayName: name);
-                    }
-                    await context.read<DatabaseService>().updateUserData(
-                          user.uid,
-                          name: name ?? user.displayName,
-                        );
-                  },
-                  child: Text(widget.submitButtonText ?? ''),
-                ),
-              ],
+              ),
             ),
           ),
-        ),
+          if (loading) Loading(),
+        ],
       ),
     );
   }
