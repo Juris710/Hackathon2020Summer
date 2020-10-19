87486a0c87cde17a6caf71abffff35ccf40a064a
Juris710 <yuu00.juris@gmail.com>
2020-10-19 22:57:05 +0900

    Webで起動できない問題修正
    Webでのアプリ名：バージョン情報取得方法模索する必要あり

--- lib/shared/app_info.dart
+++ lib/shared/app_info.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:package_info/package_info.dart';
 
 class AppInfo {
@@ -9,7 +10,18 @@ class AppInfo {
   static String _appVersion = '';
   static String _buildNumber = '';
   static String _packageName = '';
+  static Future _initializeForWeb() async {
+    //TODO：Webでのアプリ名：バージョン情報取得方法模索
+    _appName = 'Nior';
+    _appVersion = '1.0.0';
+    _buildNumber = '1';
+  }
+
   static Future initialize() async {
+    if (kIsWeb) {
+      _initializeForWeb();
+      return;
+    }
     final packageInfo = await PackageInfo.fromPlatform();
     _appName = packageInfo.appName;
     _appVersion = packageInfo.version;
